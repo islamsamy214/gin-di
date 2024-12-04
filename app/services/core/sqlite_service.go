a878b0c81acefc4d5afb92bb2b00c1dc2f47a32f
islamsamy214 <islamsamy214@gmail.com>
2024-12-04 15:15:56 +0400

    add error logging to SqliteService methods and implement transaction handling

--- app/services/core/sqlite_service.go
+++ app/services/core/sqlite_service.go
@@ -3,6 +3,8 @@ package core
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type SqliteService struct {
@@ -13,6 +15,7 @@ type SqliteService struct {
 func NewSqliteService() (*SqliteService, error) {
 	db, err := sql.Open("sqlite3", "./database/database.sqlite")
 	if err != nil {
+		log.Println("Error opening database connection", err)
 		return nil, err
 	}
 
@@ -32,42 +35,81 @@ func (s *SqliteService) Close() error {
 func (s *SqliteService) Create(query string, args ...interface{}) (sql.Result, error) {
 	stmt, err := s.db.Prepare(query)
 	if err != nil {
+		log.Println("Error preparing statement", err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	return stmt.Exec(args...)
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		log.Println("Error executing statement", err)
+		return nil, err
+	}
+
+	return res, nil
 }
 
 // Read runs a SELECT query.
 func (s *SqliteService) Read(query string, args ...interface{}) (*sql.Rows, error) {
 	stmt, err := s.db.Prepare(query)
 	if err != nil {
+		log.Println("Error preparing statement", err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	return stmt.Query(args...)
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		log.Println("Error executing statement", err)
+		return nil, err
+	}
+
+	return rows, nil
 }
 
 // Update runs an UPDATE query.
 func (s *SqliteService) Update(query string, args ...interface{}) (sql.Result, error) {
 	stmt, err := s.db.Prepare(query)
 	if err != nil {
+		log.Println("Error preparing statement", err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	return stmt.Exec(args...)
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		log.Println("Error executing statement", err)
+		return nil, err
+	}
+
+	return res, nil
 }
 
 // Delete runs a DELETE query.
 func (s *SqliteService) Delete(query string, args ...interface{}) (sql.Result, error) {
 	stmt, err := s.db.Prepare(query)
 	if err != nil {
+		log.Println("Error preparing statement", err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	return stmt.Exec(args...)
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		log.Println("Error executing statement", err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// Begin starts a transaction.
+func (s *SqliteService) Begin() (*sql.Tx, error) {
+	tx, err := s.db.Begin()
+	if err != nil {
+		log.Println("Error beginning transaction", err)
+		return nil, err
+	}
+
+	return tx, nil
 }
