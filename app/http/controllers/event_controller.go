5885ff9add38e47ebc73a587d7c3c771d401bf1b
islamsamy214 <islamsamy214@gmail.com>
2025-01-18 15:06:31 +0400

    Add event controller with index and create methods for event management

--- app/http/controllers/event_controller.go
+++ app/http/controllers/event_controller.go
@@ -1,7 +1,39 @@
 package controllers
 
+import (
+	"net/http"
+	"web-app/app/models/event"
+
+	"github.com/gin-gonic/gin"
+)
+
 type EventController struct{}
 
 func NewEventController() *EventController {
 	return &EventController{}
+}
+
+func (e *EventController) Index(c *gin.Context) {
+	eventsModel := event.NewEventModel()
+	events, err := eventsModel.Paginate(10, 1)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": events})
+}
+
+func (e *EventController) Create(c *gin.Context) {
+	eventsModel := event.NewEventModel()
+	if err := c.ShouldBindJSON(eventsModel); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	eventsModel.UserId = 1
+	if err := eventsModel.Create(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": eventsModel})
 }
