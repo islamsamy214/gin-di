09c9befd4d20f597e530a39ec091a264d1e6fad0
islamsamy214 <islamsamy214@gmail.com>
2025-03-05 01:44:35 +0200

    feat: add joho .env loader

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"log"
 	"os"
 	"web-app/app/providers"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	_ = godotenv.Load()
+
 	// Start server on http
 	if len(os.Args) > 1 && os.Args[1] == "http" {
 		providers.NewHttpServiceProvider().Boot()
