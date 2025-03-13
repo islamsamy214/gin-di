d62ec05d53cdaa2babba1fa4d9c1a66ef4a56312
islamsamy214 <islamsamy214@gmail.com>
2025-03-13 17:09:06 +0400

    feat: add constructor for ExampleCommand to facilitate instantiation

--- app/console/example_command.go
+++ app/console/example_command.go
@@ -5,6 +5,11 @@ import "fmt"
 // ExampleCommand is a sample command implementation
 type ExampleCommand struct{}
 
+// NewExampleCommand creates a new instance of ExampleCommand
+func NewExampleCommand() *ExampleCommand {
+	return &ExampleCommand{}
+}
+
 // Handle implements the command execution
 func (command *ExampleCommand) Handle(args []string) error {
 	fmt.Println("Executing example command with args:", args)
